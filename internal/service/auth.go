17da3971acbb9dc08b791cff8f19ff22657b0841
Ваше Имя <you@example.com>
2022-11-14 22:18:59 +0300

    post orders

--- internal/service/auth.go
+++ internal/service/auth.go
@@ -4,7 +4,6 @@ import (
 	"crypto/sha1"
 	"errors"
 	"fmt"
-	"strings"
 	"time"
 
 	"github.com/UserNaMEeman/shops/app"
@@ -53,8 +52,8 @@ func (s *AuthService) GenerateToken(user app.User) (string, error) {
 }
 
 func (s *AuthService) ParseToken(accessToken string) (string, error) {
-	jwtString := strings.Split(accessToken, "Bearer ")[1]
-	token, err := jwt.ParseWithClaims(jwtString, &tokenClaims{}, func(token *jwt.Token) (interface{}, error) {
+	// jwtString := strings.Split(accessToken, "Bearer ")[1]
+	token, err := jwt.ParseWithClaims(accessToken, &tokenClaims{}, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, errors.New("invalid signing method")
 		}
