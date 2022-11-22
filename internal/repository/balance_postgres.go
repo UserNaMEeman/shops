5f94b17216d259a10a0b1de69a41f4e60324067c
Ваше Имя <you@example.com>
2022-11-23 00:07:05 +0300

    check balance

--- internal/repository/balance_postgres.go
+++ internal/repository/balance_postgres.go
@@ -1,8 +1,6 @@
 package repository
 
 import (
-	"fmt"
-
 	"github.com/UserNaMEeman/shops/app"
 	"github.com/jmoiron/sqlx"
 )
@@ -16,15 +14,17 @@ func NewBalancePostgres(db *sqlx.DB) *BalancePostgres {
 }
 
 func (r *BalancePostgres) GetBalance(guid string, totalAccrual float64) (app.Balance, error) {
-	var withdrawn float64
-	balnce := app.Balance{}
-	queryOrder := fmt.Sprintf("SELECT withdrawn FROM %s WHERE user_guid = $1", balanceTable)
-	row := r.db.QueryRow(queryOrder, guid) //(queryOrder, guid)
-	if err := row.Scan(&withdrawn); err != nil {
-		return app.Balance{}, err
-	}
-	balnce.Current = totalAccrual - withdrawn
-	balnce.Withdrawn = withdrawn
-	fmt.Println("postgre balance: ", balnce)
-	return balnce, nil
+	// var withdrawn float64
+	// balnce := app.Balance{}
+	// queryOrder := fmt.Sprintf("SELECT withdrawn FROM %s WHERE user_guid = $1", balanceTable)
+	// row := r.db.QueryRow(queryOrder, guid) //(queryOrder, guid)
+	// if err := row.Scan(&withdrawn); err != nil {
+	// 	return app.Balance{}, err
+	// }
+	// balnce.Current = totalAccrual - withdrawn
+	// balnce.Withdrawn = withdrawn
+	// fmt.Println("postgre balance: ", balnce)
+	// return balnce, nil
+	a := app.Balance{totalAccrual, 0}
+	return a, nil
 }
