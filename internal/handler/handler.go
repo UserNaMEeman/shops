ee70a0de859b193d3e1656d44b08dc35cbb011bb
Ваше Имя <you@example.com>
2022-11-22 23:25:07 +0300

    check balance

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -24,9 +24,9 @@ func (h *Handler) InitRoutes() *chi.Mux {
 	router.Route("/api/user", func(router chi.Router) {
 		// router.Use(middleware.Logger)
 		router.Use(h.userIdentity)
-		router.Post("/orders", h.uploadOrder)         //загрузка пользователем номера заказа для расчёта
-		router.Get("/orders", h.GetOrders)            //получение списка загруженных пользователем номеров заказов, статусов их обработки и информации о начислениях
-		router.Get("/api/user/balance", h.GetBalance) //получение текущего баланса счёта баллов лояльности пользователя
+		router.Post("/orders", h.uploadOrder) //загрузка пользователем номера заказа для расчёта
+		router.Get("/orders", h.GetOrders)    //получение списка загруженных пользователем номеров заказов, статусов их обработки и информации о начислениях
+		router.Get("/balance", h.GetBalance)  //получение текущего баланса счёта баллов лояльности пользователя
 		//
 	})
 	// router.Post("/api/user/orders", signUp)           //загрузка пользователем номера заказа для расчёта
