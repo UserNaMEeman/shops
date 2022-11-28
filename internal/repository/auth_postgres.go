d2eb211a62106a45006f255e9fa3ba0a036b6511
Ваше Имя <you@example.com>
2022-11-28 22:41:23 +0300

    db

--- internal/repository/auth_postgres.go
+++ internal/repository/auth_postgres.go
@@ -33,6 +33,7 @@ func (r *AuthPostgres) CreateUser(ctx context.Context, user app.User) (string, e
 	}
 	// query := fmt.Sprintf("INSERT INTO %s (login, user_guid, password_hash) values ($1, $2, $3) RETURNING user_guid", usersTable)
 	row := tx.QueryRowContext(ctx, "INSERT INTO @usersTable (login, user_guid, password_hash) values (@login, @guid, @password_hash) RETURNING user_guid",
+		sql.Named("usersTable", usersTable),
 		sql.Named("login", user.Login),
 		sql.Named("guid", user.Login),
 		sql.Named("password_hash", user.Password),
@@ -43,6 +44,7 @@ func (r *AuthPostgres) CreateUser(ctx context.Context, user app.User) (string, e
 		return "", err
 	}
 	_, err = tx.ExecContext(ctx, "INSERT INTO @balanceTable (user_guid, current, withdrawn) values (@giud, @val, @val)",
+		sql.Named("balanceTable", balanceTable),
 		sql.Named("giud", user.Login),
 		sql.Named("val", 0),
 		sql.Named("val", 0),
