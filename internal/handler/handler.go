a6dca8603d709ddf35a2c2e22081d308c2c00d1d
Ваше Имя <you@example.com>
2022-11-28 20:52:55 +0300

    gzip

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -23,6 +23,7 @@ func (h *Handler) InitRoutes() *chi.Mux {
 	router.Route("/api/user", func(router chi.Router) {
 		// router.Use(middleware.Logger)
 		router.Use(h.userIdentity)
+		router.Use(h.GzipHandle)
 		router.Post("/orders", h.uploadOrder)        //загрузка пользователем номера заказа для расчёта
 		router.Get("/orders", h.getOrders)           //получение списка загруженных пользователем номеров заказов, статусов их обработки и информации о начислениях
 		router.Get("/balance", h.getBalance)         //получение текущего баланса счёта баллов лояльности пользователя
--- internal/handler/mymiddleware.go
+++ internal/handler/mymiddleware.go
@@ -1,8 +1,11 @@
 package handler
 
 import (
+	"compress/gzip"
 	"context"
+	"io"
 	"net/http"
+	"strings"
 
 	"github.com/UserNaMEeman/shops/app"
 )
@@ -23,5 +26,32 @@ func (h *Handler) userIdentity(next http.Handler) http.Handler {
 		ctx := r.Context()
 		ctx = context.WithValue(ctx, app.TypeGUID, userGUID)
 		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
+type gzipWriter struct {
+	http.ResponseWriter
+	Writer io.Writer
+}
+
+func (w gzipWriter) Write(b []byte) (int, error) {
+	return w.Writer.Write(b)
+}
+
+func (h *Handler) GzipHandle(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
+			next.ServeHTTP(w, r)
+			return
+		}
+		gz, err := gzip.NewWriterLevel(w, gzip.BestSpeed)
+		if err != nil {
+			io.WriteString(w, err.Error())
+			return
+		}
+		defer gz.Close()
+		w.Header().Set("Content-Encoding", "gzip")
+		w.Header().Set("Content-Type", "text/html")
+		next.ServeHTTP(gzipWriter{ResponseWriter: w, Writer: gz}, r)
 	})
 }
